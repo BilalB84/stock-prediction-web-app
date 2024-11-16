9efff18ed74c4c3fea16a1736c4435bc69ef14e5
Sevilay Munire Girgin, MSc. <157280744+SevilayMuni@users.noreply.github.com>
2024-11-16 22:54:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,86 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import yfinance as yf
+from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from keras.models import load_model
 
-st.title('🎈 App Name')
+apple_pickled_model = apple_model2 = tf.keras.models.load_model('app_model.h5')
 
-st.write('Hello world!')
+tab1, tab2 = st.tabs(["APPLE Stock", "GOOGLE Stock"])
+
+tab1.header('LSTM Forecast App')
+tab1.subheader('Apple Stock')
+
+# Define function to calculate 'On Balance Volume (OBV)'
+def On_Balance_Volume(Close, Volume):
+    change = Close.diff()
+    OBV = np.cumsum(np.where(change > 0, Volume, np.where(change < 0, -Volume, 0)))
+    return OBV
+
+# Select model variables
+apple_features = ['adj_close', 'garman_klass_volatility', 'dollar_volume', 'obv', 'ma_3_days']
+
+# Create scaler
+scaler = MinMaxScaler(feature_range = (0, 1))
+
+def apple_process(scaler, apple_features):
+    # Determine end and start dates for dataset download
+    end = datetime.now()
+    start = datetime(end.year, end.month - 2, end.day)
+
+    # Download Apple's dataset between start and end dates
+    apple_df = yf.download('AAPL', start=start, end=end)
+
+    # Rename columns of the Apple DataFrame
+    column_dict = {'Open': 'open', 'High': 'high', 'Low': 'low',
+                   'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}
+    apple_df = apple_df.rename(columns=column_dict)
+    apple_df.index.names = ['date']
+
+    # Add additional calculated features
+    apple_df['garman_klass_volatility'] = ((np.log(apple_df['high']) - np.log(apple_df['low'])) ** 2) / 2 - \
+                                          (2 * np.log(2) - 1) * ((np.log(apple_df['adj_close']) - np.log(apple_df['open'])) ** 2)
+    apple_df['dollar_volume'] = (apple_df['adj_close'] * apple_df['volume']) / 1e6
+    apple_df['obv'] = On_Balance_Volume(apple_df['close'], apple_df['volume'])
+    apple_df['ma_3_days'] = apple_df['adj_close'].rolling(3).mean()
+
+    # Filter and preprocess the dataset
+    apple_dset = apple_df.filter(apple_features)
+    apple_dset.dropna(axis=0, inplace=True)
+    apple_test_scaled = scaler.fit_transform(apple_dset)
+
+    return apple_test_scaled
+
+apple_dataset = apple_process(scaler, apple_features)
+
+def feed_model(dataset, n_past, model, scaler):
+    # Create X from the dataset
+    dataX = []
+    for i in range(n_past, len(dataset)):
+        dataX.append(dataset[i - n_past:i, 0:dataset.shape[1]])
+    testX = np.array(dataX)
+    
+    # Make predictions using the model
+    pred_initial = model.predict(testX)
+    
+    # Repeat predictions and reshape to original scale
+    pred_array = np.repeat(pred_initial, 5, axis = -1)
+    preds = scaler.inverse_transform(np.reshape(pred_array, (len(pred_initial), 5)))[:5, 0]
+    
+    return preds
+
+prediction = feed_model(apple_dataset, 21, apple_pickled_model, scaler).tolist()
+# create a dataframe
+pred_df = pd.DataFrame({'Predicted Day': ['Tomorrow', '2nd Day', '3rd Day', '4th Day', '5th Day'],
+                        'Adj. Closing Price($)': [ '%.2f' % elem for elem in prediction]})
+
+# set the index to the 'name' column
+pred_df.set_index('Predicted Day', inplace=True)
+
+# Display result
+tab1.info(f"Result: {pred_df}")
+
+tab1.markdown(''':rainbow[End-to-end project is done by] and :blue-background[Sevilay Munire Girgin]''')
