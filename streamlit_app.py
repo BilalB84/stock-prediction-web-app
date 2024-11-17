46af1678ceda0a6fcf5cd1bb40e79d637f272d9b
Sevilay Munire Girgin, MSc. <157280744+SevilayMuni@users.noreply.github.com>
2024-11-17 16:59:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,9 +113,9 @@ if actual_values and prediction:
 
     percent_change = (next_predicted_price - last_actual_price) / last_actual_price * 100
     insight = (
-        f"The next predicted stock price is *${next_predicted_price:.2f}*, "
-        f"compared to the last actual price of *${last_actual_price:.2f}*. "
-        f"This indicates a *{percent_change:+.2f}%* change."
+        f"The next predicted stock price is ${next_predicted_price:.2f}, "
+        f"compared to the last actual price of ${last_actual_price:.2f}. "
+        f"This indicates a {percent_change:+.2f}% change."
     )
 else:
     insight = "Not enough data to generate insights."
